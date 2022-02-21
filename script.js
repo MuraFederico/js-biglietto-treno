a0782e40410e58e381b29c4000e4acde323173af
MuraFederico <federicomura98@gmail.com>
2022-02-21 14:21:36 +0100

    esercizio funzionante ma senza controlli

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 let age = prompt('quanti anni hai?');
 let km = prompt('quanti chilometri e lungo il viaggio?');
 
+let price = km * 0.21;
+
+if (age < 18) {
+    sale = price * 20 / 100;
+    price = price - sale;
+    // price = Math.floor(price * 100) / 100;
+    document.getElementById('my-output').innerHTML = `il biglietto costa ${price.toFixed(2)}$`;    
+} else if (age > 65) {
+    sale = price * 40 / 100;
+    price = price - sale;
+    // price = Math.floor(price * 100) / 100;
+    document.getElementById('my-output').innerHTML = `il biglietto costa ${price.toFixed(2)}$`;
+} else {
+    // price = Math.floor(price * 100) / 100;
+    document.getElementById('my-output').innerHTML = `il biglietto costa ${price.toFixed(2)}$`;
+}
+
+console.log(price);
